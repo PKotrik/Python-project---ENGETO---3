cf4e07c089e6669b46ffcb8d8ca317660bfdc2ac
PKotrik <iliasovap@gmail.com>
2024-11-24 18:01:19 +0100

    Formatting and function naming changes.

--- Task_3.py
+++ Task_3.py
@@ -22,24 +22,21 @@ def eval_arg_len() -> bool:
         return True
 
 
-def eval_arg_1(odkaz: str) -> bool:
+def eval_arg_1(link: str) -> bool:
     """
     Tato funkce vyhodnoti jestli prvni argument zadan uzivatelem byl zadan spravne. Jestli ano, vrati True, jinak False.
     """
-    if (
-        odkaz.startswith("https://www.volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=")
-        and "&xnumnuts" in odkaz
-    ):
+    if link.startswith("https://www.volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=") and "&xnumnuts" in link:
         return True
     else:
         return False
 
 
-def eval_arg_2(jmeno_csv: str) -> bool:
+def eval_arg_2(name_csv: str) -> bool:
     """
     Tato funkce vyhonoti jestli druhy argument zadan uzivatelem byl zadan spravne,vcetne pripony .csv. Jestli ano, vrati True, jinak False.
     """
-    if jmeno_csv.endswith(".csv"):
+    if name_csv.endswith(".csv"):
         return True
     else:
         return False
@@ -54,7 +51,7 @@ def eval_args(arg_1: str, arg_2: str) -> bool:
     eval_1 = eval_arg_1(arg_1)
     eval_2 = eval_arg_2(arg_2)
 
-    if eval_1 == True and eval_2 == True:
+    if eval_1 and eval_2:
         print("Argumenty byly zadany spravne, stahuji data.")
         return True
 
@@ -70,128 +67,125 @@ def eval_args(arg_1: str, arg_2: str) -> bool:
 
     else:
         print(
-            "Chyba v zadani argumentu. Je potreba zada odkaz na uzemni celek s moznosti vyberu obci a jmeno souboru s priponou .csv."
+            "Chyba v zadani argumentu. Je potreba zadat nejdriv odkaz na uzemni celek s moznosti vyberu obci a pak jmeno souboru s priponou .csv."
         )
 
 
-def create_list_of_links(odkaz: str) -> list:
+def create_list_of_links(link: str) -> list:
     """
     Tato funkce prehleda vstupni web a vytvori seznam odkazu na jednotlive obce ve vybranem uzemnim celku. 
     """
-    odpoved = get(odkaz)
-    rozdelene_html = bs(odpoved.text, features="html.parser")
-    vsechny_a_odkazy = rozdelene_html.find_all("a")
+    answer = get(link)
+    split_html = bs(answer.text, features="html.parser")
+    all_links = split_html.find_all("a")
 
-    odkazy = []
-    for a_tag in vsechny_a_odkazy:
-        if a_tag.attrs["href"] in odkazy:
+    links = []
+    for a_tag in all_links:
+        if a_tag.attrs["href"] in links:
             continue
         else:
-            odkazy.append(a_tag.attrs["href"])
+            links.append(a_tag.attrs["href"])
 
-    odkazy_final = []
+    links_final = []
 
-    for link in odkazy:
+    for link in links:
         if "xvyber" in link:
-            odkazy_final.append("https://www.volby.cz/pls/ps2017nss/" + link)
+            links_final.append("https://www.volby.cz/pls/ps2017nss/" + link)
 
-    return odkazy_final
+    return links_final
 
 
-def vytvor_hlavicku(odkazy_final: list) -> list:
+def create_header(links_final: list) -> list:
     """
     Tato funkce vytvori hlavicku vystupniho .csv souboru.
     """
-    hlavicka = [
+    header = [
         "Kód obce",
         "Název obce",
         "Voliči v seznamu",
         "Vydané obálky",
         "Platné hlasy",
     ]
-    odpoved = get(odkazy_final[0])
-    rozdelene_html = bs(odpoved.text, features="html.parser")
-    vsechny_strany = rozdelene_html.find_all("td", {"class": "overflow_name"})
+    answer = get(links_final[0])
+    split_html = bs(answer.text, features="html.parser")
+    all_parties = split_html.find_all("td", {"class": "overflow_name"})
 
-    for strana in vsechny_strany:
-        strana = str(strana)
-        strana_split = strana.split(">")
-        strana_jmeno_clean = str(strana_split[1]).strip("</td>")
-        hlavicka.append(strana_jmeno_clean)
+    for party in all_parties:
+        party = str(party)
+        party_split = party.split(">")
+        party_name_clean = str(party_split[1]).strip("</td>")
+        header.append(party_name_clean)
 
-    return hlavicka
+    return header
 
 
-def cti_data_z_odkazu(odkaz: str) -> list:
+def read_data_from_link(link: str) -> list:
     """
-    Tato funkce prechazi data z vybraneho odkazu a dohledava informace definovane v hlavicce, ktere ocisti a uklada do listu 'radek'. 
+    Tato funkce prechazi data z vybraneho odkazu a dohledava informace definovane v hlavicce, ktere ocisti a uklada do listu 'row'. 
     """
-    radek = []
-    kod_obce = odkaz.split("&x")
-    radek.append(kod_obce[2].strip("obec="))
-    odpoved = get(odkaz)
-    rozdelene_html = bs(odpoved.text, features="html.parser")
-    nazvy = rozdelene_html.find_all("h3")
-    data = rozdelene_html.find_all("td", {"class": "cislo"})
+    row = []
 
-    for nazev in nazvy:
-        if "Obec:" in str(nazev):
-            nazev_split = str(nazev).split("Obec: ")
-            radek.append(nazev_split[1].strip("\n</h3>"))
+    town_code = link.split("&x")
+    row.append(town_code[2].strip("obec="))
+
+    answer = get(link)
+    split_html = bs(answer.text, features="html.parser")
+
+    town_names = split_html.find_all("h3")
+    data = split_html.find_all("td", {"class": "cislo"})
+
+    for name in town_names:
+        if "Obec:" in str(name):
+            name_split = str(name).split("Obec: ")
+            row.append(name_split[1].strip("\n</h3>"))
         else:
             continue
 
-    for vysledek in data:
-        vysledek_split = str(vysledek).split(">")
-        vysledek_clean = vysledek_split[1].strip("</td>")
+    for result in data:
+        result_split = str(result).split(">")
+        result_clean = result_split[1].strip("</td>")
 
-        if "sa2" in vysledek.attrs["headers"]:
-            radek.append(vysledek_clean)
-        elif "sa3" in vysledek.attrs["headers"]:
-            radek.append(vysledek_clean)
-        elif "sa6" in vysledek.attrs["headers"]:
-            radek.append(vysledek_clean)
-        elif (
-            "t1sa2" in vysledek.attrs["headers"]
-            and "t1sb3" in vysledek.attrs["headers"]
-        ):
-            radek.append(vysledek_clean)
-        elif (
-            "t2sa2" in vysledek.attrs["headers"]
-            and "t2sb3" in vysledek.attrs["headers"]
-        ):
-            radek.append(vysledek_clean)
+        if "sa2" in result.attrs["headers"]:
+            row.append(result_clean)
+        elif "sa3" in result.attrs["headers"]:
+            row.append(result_clean)
+        elif "sa6" in result.attrs["headers"]:
+            row.append(result_clean)
+        elif "t1sa2" in result.attrs["headers"] and "t1sb3" in result.attrs["headers"]:
+            row.append(result_clean)
+        elif "t2sa2" in result.attrs["headers"] and "t2sb3" in result.attrs["headers"]:
+            row.append(result_clean)
         else:
             continue
 
-    return radek
+    return row
 
 
-def zapis_data(jmeno_csv: str, odkazy_final: list):
+def write_data(name_csv: str, links_final: list):
     """
     Tato funkce zapise hlavicku a jednotlive radky do souboru .csv.
     """
-    with open(jmeno_csv, mode="w", encoding="utf-16") as csv_soubor:
-        hlavicka = vytvor_hlavicku(odkazy_final)
-        zapisovac = csv.writer(csv_soubor, dialect="excel-tab", lineterminator="\n")
-        zapisovac.writerow(hlavicka)
+    with open(name_csv, mode="w", encoding="utf-16") as csv_file:
+        header = create_header(links_final)
+        writer = csv.writer(csv_file, dialect="excel-tab", lineterminator="\n")
+        writer.writerow(header)
 
-        for odkaz in odkazy_final:
-            zapisovac.writerow(cti_data_z_odkazu(odkaz))
+        for odkaz in links_final:
+            writer.writerow(read_data_from_link(odkaz))
 
-    return print("Vsechna data zapsana.")
+    print("Vsechna data zapsana.")
 
 
 def main():
     """
     Tato funkce vyhodnoti jestli argumenty splnuji vsechny pozadavky, jestli ano, spusti funkci na vytvoreni zoznamu odkazu a nasledne spusti proces zapisu dat do .csv.
     """
-    eval_arg_len()
     if eval_arg_len() == True:
         if eval_args(sys.argv[1], sys.argv[2]) == True:
-            odkazy_final = create_list_of_links(sys.argv[1])
-            zapis_data(sys.argv[2], odkazy_final)
+            links_final = create_list_of_links(sys.argv[1])
+            write_data(sys.argv[2], links_final)
 
 
-main()
+if __name__ == "__main__":
+    main()
 
